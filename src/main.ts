6a00697c014f76bf06318a4a1e280a9185ffbe23
zbn <129472520+qwerzbn@users.noreply.github.com>
2025-01-20 14:31:02 +0800

    优化主页面

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import Antd from 'ant-design-vue'
 import 'ant-design-vue/dist/reset.css'
 import App from './App.vue'
 import router from './router'
+import '../global.css'
 
 const app = createApp(App)
 
--- src/pages/HomeView.vue
+++ src/pages/HomeView.vue
@@ -7,28 +7,67 @@
       :pagination="pagination"
       :loading="loading"
     >
-      <template #renderItem="{ item: audio }">
+      <template #renderItem="{ item: audio, index }">
         <a-list-item style="padding: 18px">
           <!-- 单张图片 -->
-          <a-card hoverable>
+          <a-card hoverable class="homeCard">
             <template #cover>
               <img
-                style="height: 180px;  border-radius: 20px;object-fit: cover"
+                style="height: 180px; border-radius: 20px; object-fit: cover"
                 :alt="audio.picture"
                 :src="audio.picture"
               />
             </template>
-            <a-card-meta :title="audio.title">
+            <a-card-meta style="padding: -12px">
               <template #description>
-                {{ audio.description }}
-                <!--                <a-flex>-->
-                <!--                  <a-tag color="green">-->
-                <!--                    {{ audio.category ?? '默认' }}-->
-                <!--                  </a-tag>-->
-                <!--                  <a-tag v-for="tag in picture.tags" :key="tag">-->
-                <!--                    {{ tag }}-->
-                <!--                  </a-tag>-->
-                <!--                </a-flex>-->
+                <a-row>
+                  <a-col flex="30px">
+                    <!-- 播放图标 -->
+                    <div @click="togglePlay(index)">
+                      <span v-if="!isPlaying[index]"
+                        ><PlayCircleOutlined style="font-size: 40px"
+                      /></span>
+                      <!-- 播放图标 -->
+                      <span v-else><PauseCircleOutlined style="font-size: 40px" /></span>
+                      <!-- 暂停图标 -->
+                    </div>
+                    <audio
+                      ref="audioPlayer"
+                      :src="audio.filePath"
+                      @ended="onAudioEnded(index)"
+                    ></audio>
+                  </a-col>
+                  <a-col flex="auto">
+                    <a-flex style="align-items: center; margin-left: 12px; margin-top: 12px">
+                      <div v-for="tag in audio.tags">
+                        <a-tag
+                          v-if="tag === '中文'"
+                          color="blue"
+                          style="align-items: center"
+                          :key="tag"
+                        >
+                          {{ tag }}
+                        </a-tag>
+                        <a-tag
+                          v-if="tag === '女声'"
+                          color="green"
+                          style="align-items: center"
+                          :key="tag"
+                        >
+                          {{ tag }}
+                        </a-tag>
+                        <a-tag
+                          v-if="tag === '舒缓'"
+                          color="orange"
+                          style="align-items: center"
+                          :key="tag"
+                        >
+                          {{ tag }}
+                        </a-tag>
+                      </div>
+                    </a-flex>
+                  </a-col>
+                </a-row>
               </template>
             </a-card-meta>
           </a-card>
@@ -43,10 +82,28 @@
 import { computed, onMounted, reactive, ref } from 'vue'
 import { message } from 'ant-design-vue'
 import { listAudioFileVoByPageUsingPost } from '@/api/audioFileController.ts'
+import { PauseCircleOutlined, PlayCircleOutlined } from '@ant-design/icons-vue'
 
 const dataList = ref<API.AudioFileVO[]>([])
 const total = ref(0)
 const loading = ref(true)
+const audioPlayer = ref<HTMLAudioElement | null>(null)
+const isPlaying = ref<boolean[]>(dataList.value.map(() => false))
+
+// 切换播放/暂停
+const togglePlay = (index: number) => {
+  if (isPlaying.value[index]) {
+    audioPlayer.value?.pause() // 暂停
+  } else {
+    audioPlayer.value?.play() // 播放
+  }
+  isPlaying.value[index] = !isPlaying.value[index] // 切换状态
+}
+
+// 音频播放结束时的回调
+const onAudioEnded = (index: number) => {
+  isPlaying.value[index] = false // 播放结束，更新状态
+}
 
 // 搜索条件
 const searchParams = reactive<API.AudioFileQueryRequest>({
@@ -89,3 +146,9 @@ onMounted(() => {
   fetchData()
 })
 </script>
+<style scoped>
+.ant-card .ant-card-body {
+  padding: 18px !important;
+  border-radius: 0 0 8px 8px;
+}
+</style>
